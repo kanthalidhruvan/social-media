8f84fa1e86d48e5041c25790eff966da9b61b0d1
kanthalidhruvan <kanthalidhruvan17@gmail.com>
2025-03-04 14:33:03 +0530

    project 5

--- script.js
+++ script.js
@@ -1,41 +1,104 @@
-let users = [];
+let userId;
 function showLogin() {
-  fetch("https://jsonplaceholder.typicode.com/users")
+  fetch("https://jsonplaceholder.typicode.com/users/")
     .then((res) => res.json())
     .then((data) => displayUsers(data))
     .catch((err) => console.log(err));
 }
 
-function showhome()
-{
-  let str=`
-  <div container='container-fluid'>
-    <div class='row'>
-      <div class='d-flex justify-content-between bg-primary text-light'>
-      <div>MY Social Media</div>
-      <div>Username</div>
-      </div>
-    </div>
-    <div class='row'>
-     <div class='d-flex'>
-      <div class='p-2'>
-      <p>Home</p>
-      <p>Album</p>
-      <p onclick='showLogin()'>Log out</p>
-      </div>
-      <div class='p-2'>Content</div>
-    </div>
-    </div>
-     <div class='row'>
-     <div class='bg-primary text-light p-5'>
-       <p>@Copyright 2025.All Rights Reserved.</P>
-    </div>
-    </div>
-  </div>
-  `;
-  root.innerHTML=str
+function showPosts(id) {
+  let str = "<h3>My Post</h3>";
+  fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data &&
+        data.map((value) => {
+          str += `<div>
+        <b>${value.title}</b>
+        <p>${value.body}</p>
+        </div>`;
+        });
+      content.innerHTML = str;
+    })
+    .catch((err) => console.log(err));
 }
 
+function showAlbum(id) {
+  fetch(`https://jsonplaceholder.typicode.com/albums/?userId=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let str = `<h3>My Albums</h3>`;
+      data &&
+        data.map((value) => {
+          str += `<div>${value.title}</div>`;
+        });
+      content.innerHTML = str;
+    });
+}
+
+
+function showTodos(id) {
+  fetch(`https://jsonplaceholder.typicode.com/todos/?userId=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let str = `<h3>My Todos</h3>`;
+      data &&
+        data.map((value) => {
+          str += `<div><input type='checkbox' ${value.completed && "checked"}>${value.title}</div>`;
+        });
+      content.innerHTML = str;
+    });
+}
+
+
+
+function showProfile(id) {
+  fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let str = `<h3>My Profile</h3><div>
+      <b>${data.name}</b>
+      <p>${data.email}</p>
+      </div>`;
+      content.innerHTML = str;
+    })
+    .catch((err) => console.log(err));
+}
+
+function showHome() {
+  userId = selUser.value;
+  let str = `
+   <div class='container-fluid'>
+     <div class='row'>
+      <div class='d-flex justify-content-between bg-primary text-light'>
+       <div>My Social Media</div>
+       <div id='username'></div>
+      </div>
+     </div>
+     <div class='row'>
+      <div class='d-flex'>
+       <div class='p-2'>
+         <p onclick='showPosts(${userId})'>Home</p>
+         <p onclick='showAlbum(${userId})'>Album</p>
+          <p onclick='showTodos(${userId})'>Todos</p>
+          <p onclick='showProfile(${userId})'>Profile</p>
+         <p onclick='showLogin()'>Logout</p>
+       </div>
+       <div class='p-2' id='content'></div>
+      </div>
+     </div>
+     <div class='row'>
+      <div class='bg-primary text-light p-5'>
+       <p>@Copyright 2025. All rights reserved.</p>
+      </div>
+     </div>
+   </div>
+  `;
+  let name = selUser.options[selUser.selectedIndex].text
+  root.innerHTML = str;
+  username.innerHTML = name
+  showPosts(userId);
+}
 
 function displayUsers(data) {
   let str = `
@@ -45,11 +108,11 @@ function displayUsers(data) {
   <p>This is the caption of the website.</p>
   </div>
   <div class='p-5'>
-  <select class='form-control'>
-  <option value='0'>--select user--</option>`;
+  <select class='form-control m-3' id='selUser'>
+  <option value='0'>--Select User--</option>`;
   data.map((value) => {
     str += `<option value=${value.id}>${value.name}</option>`;
   });
-  str += `</select><p><button class='form-control m-3 btn btn-primary' onclick='showhome()'>LOG IN</button></P></div></div>`
-  root.innerHTML = str
+  str += `</select><p><button class='form-control m-3 btn btn-primary' onclick='showHome()'>Log In</button></p></div></div>`;
+  root.innerHTML = str;
 }
